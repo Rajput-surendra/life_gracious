bae4dd067602ca8e1ffe47d9c1a91bba61062091
dminj <dminj22@gmail.com>
2022-03-05 17:55:05 +0530

    Initial commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,23 @@
+final String appName = 'Maangos User';
+
+final String packageName = 'com.maangos.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.maangos.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = '123456789';
+
+final String deepLinkUrlPrefix = 'https://eshopmultivendor.page.link';
+final String deepLinkName = 'eshop';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+//final String baseUrl = 'https://vendor.eshopweb.store/app/v1/api/';
+// final String baseUrl = 'https://alphawizztest.tk/Maangos_Ecom/app/v1/api/';
+final String baseUrl = 'https://maangos.in/app/v1/api/';
+// final String imageUrl = 'https://alphawizztest.tk/Maangos_Ecom/';
+final String imageUrl = 'https://maangos.in/';
+
+
+final String jwtKey = "78084f1698c9fcff5a668b68dcd103db39be2605";
